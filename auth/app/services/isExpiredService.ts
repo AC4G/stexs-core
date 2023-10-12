01d0c34f7493bf1fb279c7ce7ce6df16e54d4a8b
AC4G <vost_400@gmx.de>
2023-10-13 01:33:40 +0200

    first commit as monorepo

--- auth/app/services/isExpiredService.ts
+++
@@ -1,5 +0,0 @@
-export default function isExpired(date: string, minutes: number): boolean {
-    const expiryDate = new Date(date);
-    expiryDate.setMinutes(expiryDate.getMinutes() + minutes);
-    return expiryDate < new Date();
-}
