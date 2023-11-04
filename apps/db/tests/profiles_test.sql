63abca39ea01f7b5baf2d7ee88a8ae458578e1f4
AC4G <vost_400@gmx.de>
2023-11-05 00:23:31 +0100

    added aditional tests for profiles table

--- apps/db/tests/profiles_test.sql
+++ apps/db/tests/profiles_test.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT plan(7);
+SELECT plan(13);
 
 SELECT ok(
     EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'profiles'),
@@ -43,4 +43,32 @@ SELECT is(
     'CHECK constraint is valid'
 );
 
-COMMIT;
+SELECT ok(
+    has_column_privilege('authenticated', 'public.profiles', 'username', 'UPDATE') AND
+    has_column_privilege('authenticated', 'public.profiles', 'is_private', 'UPDATE') AND
+    has_column_privilege('authenticated', 'public.profiles', 'friend_privacy_level', 'UPDATE') AND
+    has_column_privilege('authenticated', 'public.profiles', 'inventory_privacy_level', 'UPDATE'),
+    'authenticated role has UPDATE privilege on specified columns'
+);
+
+SELECT ok(
+    has_table_privilege('anon', 'public.profiles', 'SELECT'),
+    'anon role has SELECT privilege on public.profiles'
+);
+
+SELECT ok(
+    has_table_privilege('authenticated', 'public.profiles', 'SELECT'),
+    'authenticated role has SELECT privilege on public.profiles'
+);
+
+SELECT col_is_pk('profiles', 'user_id', 'user_id is a Primary Key');
+
+SELECT col_is_unique('profiles', 'username', 'username has a unique constraint');
+
+SELECT is(
+    (SELECT conname FROM pg_constraint WHERE conname = 'profiles_check' AND conrelid = 'public.profiles'::regclass),
+    'profiles_check',
+    'CHECK constraint is valid'
+);
+
+ROLLBACK;
