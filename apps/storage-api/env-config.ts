9967f85cf3114beed08b1084c0c9ebdd56c6d5cf
AC4G <vost_400@gmx.de>
2023-11-21 19:43:32 +0100

    added a Dockerfile for the storage-api

--- apps/storage-api/env-config.ts
+++ apps/storage-api/env-config.ts
@@ -3,5 +3,5 @@ import { config } from 'dotenv';
 config();
 
 export const ENV: string = process.env.ENV!;
-export const SERVER_PORT: number = parseInt(process.env.SERVER_PORT!);
+export const SERVER_PORT: number = parseInt(process.env.STORAGE_SERVER_PORT!);
 export const LOGGER_URL: string = process.env.LOGGER_URL!;
